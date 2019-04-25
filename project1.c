0281747a470d85885d6e5ddd62c92361a19a0a5c
Matthew Betts <m.betts@uon.edu.au>
2019-04-25 08:35:12 +0000

    Better

--- project1.c
+++ project1.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdio.h>
+
+
+
 
 int main() {
-    char alphabet[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'}; 
+    char alphabet[27] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'}; 
     char message[100];
     char character;
+    char SubstituitionKey[27];
     int key, i;
     int operation;
     
@@ -14,21 +19,35 @@ int main() {
     printf("4.Decryption of a message encrypted with a substitution cipher given chipher text and substitutions\n");
     printf("5.Decription of a message encrypted with a rotaion cipher given text only\n");
     printf("6.Decription of a message encrypted with a substitution cipher given cipher text only\n");
+    printf("Enter an operation\n");
     
-    scanf("%d", &operation);
+    scanf("%d", &operation);// reads int operation to determine which switch case to use
     
-    printf("Read %d\n", operation);
+    printf("Read %d\n", operation);// prints which case is being run
     
+    if(operation < 1 || operation > 6 ) {
+        printf("Enter operation between 1 and 6\n");
+        //scanf("%d",&operation);
+    }
+    
+    else if(operation == 1 || operation == 2) {
+        printf("Enter Message\n");//Message for Encription/Decription
+        scanf("%s", message); //reads message and stores in string
+        printf("Enter key\n"); //Rotation key
+        scanf("%d", &key); //reads key and stores to an int for later use
+        
+    }
+    
+    if(operation == 3 || operation == 4) {
+        printf("Enter Message\n");
+        scanf("%s", message);
+        printf("Enter Key\n");
+        scanf("%s", SubstituitionKey);
+    }
     switch(operation) {
-        case 1: //operation 1
         
+    case 1: //operation 1 Rot Encription
         
-    printf("Enter Message\n");//Message for Encription/Decription
-    scanf("%s", message);
-    printf("Enter key\n");//Rotation key
-    scanf("%d", &key);
-    
-    
         for(i = 0; message[i]!='\0';++i) {
             character = message[i];
 		
@@ -37,10 +56,10 @@ int main() {
 		}
 			else if(character > 'Z') {
 			    
-				character = character - 'Z' + 'A' - 1;
+				character = character + 'Z' + 'A' - 1 ;
 			}
 			
-			message[i] = character;
+			message[i] = character ;
         }
     printf("Encrypted Message:%s", message);
     
@@ -48,12 +67,7 @@ int main() {
         
             break;
             
-        case 2: //operation 2
-        
-    printf("Enter Message\n");//Message for Encription/Decription
-    scanf("%s", message);
-    printf("Enter key\n");//Rotation key
-    scanf("%d", &key);
+    case 2: //operation 2 Rot Decriptio
         
         
         for(i = 0;message[i]!= '\0';i++) {
@@ -70,29 +84,50 @@ int main() {
          
             message[i] = character;  
             
-    }
+        }
         
-    printf("Decrypted Message: %s", message);
-        
-            break;
-            
-        case 3: //operation 3
+        printf("Decrypted Message: %s", message);
         
             break;
             
-        case 4: //operation 4
+            
+         {
+            
+            
+            
+        }
+            
+    case 3: //operation 3 Sub Encription
+    
+    
+    for(i = 0; message[i]!='\0';++i) 
+    {
+        character = message[i];
+    
+        if(character >='A' && character <= 'Z') {
+            
+            character = SubstituitionKey[character -'A'];
+        }          
+            character = message[i];
+        
+    } 
+        printf("Encrypted Message: %s", message);
         
             break;
             
-        case 5: //operation 5
+    case 4: //operation 4 Sub Decription
         
             break;
             
-        case 6: //operation 6
+    case 5: //operation 5 Rot Attack
         
             break;
             
-        default: return 0;
+    case 6: //operation 6 Sub Attack
+        
+            break;
+            
+    default: return 0;
     }
     
     
